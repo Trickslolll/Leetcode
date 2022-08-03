11ecc16f57543761c0bdada4e055518a8a3d9c02
Trickslolll <99582012+Trickslolll@users.noreply.github.com>
2022-08-03 22:53:30 +0800

    03/28/2022

---
+++ 509. Fibonacci Number/fib.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)
+            return 0;
+        else
+        {
+            int previous = 0 , now = 1;
+            for(int i = 1; i < n; i++)
+            {
+                int next = previous + now;
+                previous = now;
+                now = next;
+            }
+            return now;
+        }
+    }
+};
