95122e4a5dae15da452d21705d4c714fff60e9ce
Trickslolll <99582012+Trickslolll@users.noreply.github.com>
2022-08-03 22:19:02 +0800

    1. Two Sum

--- twoSum.c
+++ twoSum.c
@@ -2,7 +2,6 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    
     for(int i = 0;i < numsSize ; i++){
         for(int j = 0 ; j < numsSize ; j++){
             if(i == j){continue;}
